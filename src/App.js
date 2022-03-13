6622be7cbc061a33eb74d9040ffc9c3cdc3aed5f
Milad_Mrk <milad.rastgoou@gmail.com>
2022-03-13 22:20:50 +0330

    R_milad

--- src/App.js
+++ src/App.js
@@ -1,122 +1,90 @@
-// import React, { Component } from "react";
-// import "./App.css";
-// import Menu from "./Components/Menu/Menu";
-// import ProductList from "./Components/Products/ProductList";
-// import ClassCounter from "./Components/ClassCounter";
-// import CounterFunction from "./Components/CounterFunction";
-// import Wrapper from "./Components/hoc/Wrapper";
+import React, { useState } from "react";
+import "./App.css";
+import Menu from "./Components/Menu/Menu";
+import ProductList from "./Components/Products/ProductList";
+import Wrapper from "./Components/hoc/Wrapper";
 
-// // class App extends Component {
-// //   state = {};
-// //   render() {
-// //     return (
-// //       <div>
-// //         <ClassCounter />
-// //         {/* <CounterFunction /> */}
-// //         <Wrapper />
-// //       </div>
-// //     );
-// //   }
-// // }
+export const UserContext = React.createContext();
 
-// // export default App;
-// export const UserContext = React.createContext();
+const App = () => {
+  const [item, setItem] = useState([
+    { title: "Book 1", price: "200$", id: 0, quantity: 1 },
+    { title: "Book 2", price: "300$", id: 1, quantity: 1 },
+    { title: "Book 3", price: "100$", id: 2, quantity: 1 },
+    { title: "Book 4", price: "500$", id: 3, quantity: 1 },
+  ]);
 
-// class App extends Component {
-//   state = {
-//     product: [
-//       { title: "Book 1", price: "200$", id: 0, quantity: 1 },
-//       { title: "Book 2", price: "300$", id: 1, quantity: 1 },
-//       { title: "Book 3", price: "100$", id: 2, quantity: 1 },
-//       { title: "Book 4", price: "500$", id: 3, quantity: 1 },
-//     ],
-//   };
+  const textInputHandler = (e, id) => {
+    const findIndex = item.findIndex((item) => {
+      return item.id === id;
+    });
+    const selectProduct = { ...item[findIndex] };
+    selectProduct.title = e.target.value;
+    const products = [...item];
+    products[findIndex] = selectProduct;
+    console.log(products);
+    setItem(products);
+  };
 
-//   textInputHandler = (e, id) => {
-//     // const findChange = this.state.product.find((item) => {
-//     //   return item.id === id;
-//     // });
-//     // findChange.title = e.target.value;
-//     // this.setState({ ...this.state.product, findChange });
-//     const findIndex = this.state.product.findIndex((item) => {
-//       return item.id === id;
-//     });
-//     const selectProduct = { ...this.state.product[findIndex] };
-//     selectProduct.title = e.target.value;
-//     const products = [...this.state.product];
-//     products[findIndex] = selectProduct;
-//     this.setState({ product: products });
-//   };
+  const deleteHandler = (id) => {
+    const filterProducts = item.filter((item) => {
+      return item.id !== id;
+    });
+    setItem(filterProducts);
+  };
 
-//   deleteHandler = (id) => {
-//     const filterProducts = this.state.product.filter((item) => {
-//       return item.id !== id;
-//     });
-//     this.setState({ ...this.state.product, product: filterProducts });
-//   };
+  const quantityAddHandler = (id) => {
+    const findIndexProduct = item.findIndex((item) => {
+      return item.id === id;
+    });
+    console.log(findIndexProduct);
+    const productByIndex = { ...item[findIndexProduct] };
+    productByIndex.quantity++;
+    const products = [...item];
+    products[findIndexProduct] = productByIndex;
+    setItem(products);
+  };
 
-//   quantityAddHandler = (id) => {
-//     const findIndexProduct = this.state.product.findIndex((item) => {
-//       return item.id === id;
-//     });
-//     console.log(findIndexProduct);
-//     const productByIndex = { ...this.state.product[findIndexProduct] };
-//     productByIndex.quantity++;
-//     const products = [...this.state.product];
-//     products[findIndexProduct] = productByIndex;
-//     this.setState({ product: products });
-//   };
-//   fun;
-//   quantityMinusHandler = (id) => {
-//     let indexProduct = this.state.product.findIndex((item) => {
-//       return item.id === id;
-//     });
-//     const product = { ...this.state.product[indexProduct] };
+  const quantityMinusHandler = (id) => {
+    let indexProduct = item.findIndex((item) => {
+      return item.id === id;
+    });
+    const selectProduct = { ...item[indexProduct] };
 
-//     if (product.quantity <= 1) {
-//       const filterProducts = this.state.product.filter((item) => {
-//         return item.id !== id;
-//       });
-//       this.setState({ ...this.state.product, product: filterProducts });
-//     } else {
-//       product.quantity--;
-//       const products = [...this.state.product];
-//       products[indexProduct] = product;
-//       this.setState({ product: products });
-//     }
-//   };
+    if (selectProduct.quantity <= 1) {
+      const filterProducts = item.filter((item) => {
+        return item.id !== id;
+      });
+      setItem(filterProducts);
+    } else {
+      selectProduct.quantity--;
+      const products = [...item];
+      products[indexProduct] = selectProduct;
+      setItem(products);
+    }
+  };
 
-//   componentDidUpdate(prevProps, prevState) {
-//     console.log("app.js CDP");
-//     console.log(prevState);
-//   }
-//   //WARNING! To be deprecated in React v17. Use componentDidMount instead.
+  return (
+    <div>
+      <UserContext.Provider value={"milad rastgooooooo"}>
+        <header>
+          <Menu card={item.length} />
+        </header>
+        <section>
+          <ProductList
+            product={item}
+            textInput={textInputHandler}
+            plusBtn={quantityAddHandler}
+            minusBtn={quantityMinusHandler}
+            deleteBtn={deleteHandler}
+          />
+        </section>
+      </UserContext.Provider>
+    </div>
+  );
+};
 
-//   render() {
-//     console.log("app.js render");
-//     return (
-//       <div>
-//         {/* <UserContext.Provider value={"milad rastgooooooo"}>
-//           <header>
-//             <Menu card={this.state.product.length} />
-//           </header>
-//           <section>
-//             <ProductList
-//               product={this.state.product}
-//               textInput={this.textInputHandler}
-//               plusBtn={this.quantityAddHandler}
-//               minusBtn={this.quantityMinusHandler}
-//               deleteBtn={this.deleteHandler}
-//             />
-//           </section>
-//         </UserContext.Provider> */}
-
-//       </div>
-//     );
-//   }
-// }
-
-// export default Wrapper(App, "container");
+export default Wrapper(App, "container");
 
 // import React, { Component } from "react";
 // import HocBTN from "./Components/hoc/testHOC/HocBTN";
@@ -136,20 +104,23 @@
 
 // export default App;
 
-import React, { useContext } from "react";
-import CounterOne from "./Components/Context/CounterOne";
+/// مبحث context , useReducer
+// import React, { useContext } from "react";
+// import CounterOne from "./Components/Context/CounterOne";
 
-import CounterProvider from "./Components/Context/CounterProvider";
+// import CounterProvider from "./Components/Context/CounterProvider";
+// import CounterReducer from "./Components/CounterReducer";
 
-const App = () => {
-  return (
-    <div>
-      Hi
-      <CounterProvider>
-        <CounterOne />
-      </CounterProvider>
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div>
+//       Hi
+//       <CounterProvider>
+//         <CounterOne />
+//         <CounterReducer />
+//       </CounterProvider>
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
